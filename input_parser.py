500919caf1efcb9ff4e87dac7777e5f5acc042ff
xesam <xesamguo@gmail.com>
2022-05-24 09:55:06 +0800

    input /fix

--- input_parser.py
+++ input_parser.py
@@ -5,19 +5,20 @@ from pdfs.pages import RangePageCollection
 RE_SPLITS = re.compile(',|，|\s+')
 
 
-def clean_paginations(paginations: str):
+def _clean_paginations(paginations: str):
     return [pagination for pagination in RE_SPLITS.split(paginations) if len(pagination.strip()) != 0]
 
 
-def parse_page(pagination: str):
+def _parse_page(pagination: str):
     pages = [int(i) for i in pagination.split('-')]
+    pages.sort()
     return RangePageCollection(pages[0], pages[-1])
 
 
 def parse_pages(paginations: str):
-    pages = [parse_page(i) for i in clean_paginations(paginations)]
-    return pages
+    return [_parse_page(i) for i in _clean_paginations(paginations)]
 
 
 def parse_starts(paginations: str):
-    return [int(i) for i in clean_paginations(paginations)]
+    starts = [int(i) for i in _clean_paginations(paginations)]
+    return sorted(starts)
